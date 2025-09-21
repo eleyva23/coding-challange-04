f9198fc1b0b91dbe2c3e89afcc0971d176783037
emma <eleyva2@usf.edu>
2025-09-20 21:24:24 -0700

    step 3

--- cc_4.js
+++ cc_4.js
@@ -1,8 +1,21 @@
 const products = [
    {sku: "A-110", name: "hairbrish" , category: "hair care", price: 15.25, inventory: 10},
-   {sku:"B-115", name: "lotion", category: "body care", price: 12.32, inventory: 9},
+   {sku:"B-115", name: "lotion", category: "household", price: 12.32, inventory: 9},
    {sku: "C-120", name: "iphone charger", category: "electronics", price: 25.75, inventory: 12},
    {sku: "D-125", name: "blush", category:"makeup", price: 13.75, inventory: 5},
-   {sku: "E-130", name: "towels", category: "toiletres", price: 10.75, inventory: 8},
-   
-]
+   {sku: "E-130", name: "towels", category: "household", price: 10.75, inventory: 8},
+
+];
+// Step 3 Category Discounts
+for (let p of products) {
+  let discount = 0;
+  switch (p.category) {
+    case "hair care": discount = 0.20; break;
+    case "body care":     discount = 0.15; break;
+    case "electronics":
+    case "household":   discount = 0.10; break;
+    default: discount = 0; break;
+  }
+  p.promoPrice = (p.price * (1 - discount)).toFixed(2);
+}
+// Step 4 Creating a Variable
