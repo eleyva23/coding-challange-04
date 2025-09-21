9bf1396275bcaf350281c5b076f4e6c9a6ad5c2d
emma <eleyva2@usf.edu>
2025-09-20 21:15:29 -0700

    step 2 complete creating an array

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+const products = [
+   {sku: "A-110", name: "hairbrish" , category: "hair care", price: 15.25, inventory: 10},
+   {sku:"B-115", name: "lotion", category: "body care", price: 12.32, inventory: 9},
+   {sku: "C-120", name: "iphone charger", category: "electronics", price: 25.75, inventory: 12},
+   {sku: "D-125", name: "blush", category:"makeup", price: 13.75, inventory: 5},
+   {sku: "E-130", name: "towels", category: "toiletres", price: 10.75, inventory: 8},
+   
+]
